4332612376abed9c04d3d7ae2c1a69e80ac765c3
nesteruk-yuliya <yuliya.nesteruk@vrpconsulting.com>
2025-09-29 08:11:30 +0300

    task4

--- src/task4.java
+++ src/task4.java
@@ -67,19 +67,19 @@ public class task4 {
 //  "Результат деления равен 5" В других случаях вывести: "Результат деления равен дробному числу".
 //  При этом в последнем случае вывести точный результат деления (использовать приведение типов)
 //  Для выполнения задания использовать оператор switch- case
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Введи x");
-        int x = in.nextInt();
-        float result = 15 / (float)x;
-
-        switch (15 / x) {
-            case 3: System.out.println("Результат деления равен 3");
-            break;
-            case 5: System.out.println("Результат деления равен 5");
-            break;
-            default: System.out.println("Результат деления равен дробному числу " + result);
-        }
-
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Введи x");
+//        int x = in.nextInt();
+//        float result = 15 / (float)x;
+//
+//        switch (15 / x) {
+//            case 3: System.out.println("Результат деления равен 3");
+//            break;
+//            case 5: System.out.println("Результат деления равен 5");
+//            break;
+//            default: System.out.println("Результат деления равен дробному числу " + result);
+//        }
+//    }
     }
 }
