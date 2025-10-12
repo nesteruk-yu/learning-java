3cab81f4eac5b9893878c960a8bc263f132c068d
nesteruk-yuliya <nesteruk.yu@gmail.com>
2025-10-12 23:32:40 +0300

    task8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+
+    //1.3 В методе main вызовите этот метод, передайте любые строки, в качестве аргументов и выведите результат вконсоль.
     public static void main(String[] args) {
-//        task1.execute();
-        task3.execute();
+        String1 input = new String1();
+        String resultInput = input.returnString("абра кадабра", "када");
+        System.out.println(resultInput);
+
+        String1 text = new String1();
+        String resultText = text.cutText(3, "strings");
+        System.out.println(resultText);
     }
+
 }
